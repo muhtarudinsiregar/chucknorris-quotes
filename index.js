deae37e5708eacaf8364cb5cb8348ffe7a35f641
muhtarudinsiregar <muhtarudinsiregar93@gmail.com>
2017-04-26 21:26:23 +0700

    Add skeleton function

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict'
+
+function getRandomJoke() {
+
+}
+
+function getRandomJokes() {
+
+}
+
+function getNumberOfJokes() {
+
+}
+
+function getCategories() {
+
+}
+
+// export the module
+module.exports = {
+    getRandomJoke: getRandomJoke,
+    getRandomJokes: getRandomJokes,
+    getNumberOfJokes: getNumberOfJokes,
+    getCategories: getCategories
+};
