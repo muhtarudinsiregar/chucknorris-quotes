b9b61a56057dadd5f61015bc2e1cf880c653758b
muhtarudinsiregar <muhtarudinsiregar93@gmail.com>
2017-04-27 07:34:56 +0700

    Add skeleton file for test

---
+++ tests.js
@@ -0,0 +1,20 @@
+const assert = require('assert')
+const CNJokes = require('./index.js')
+
+describe('get chucknorris joke', function() {
+  it('should return random joke', function() {
+
+  });
+
+  it('should return count jokes', function() {
+
+  });
+
+  it('should return random jokes', function() {
+
+  });
+
+  it('should return categories', function() {
+
+  });
+});
