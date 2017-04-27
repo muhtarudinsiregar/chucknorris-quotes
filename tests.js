8b8ad4a293794062df8a3bbca2110e0719138b80
muhtarudinsiregar <muhtarudinsiregar93@gmail.com>
2017-04-28 06:33:44 +0700

    Fix codestyle

--- tests.js
+++ tests.js
@@ -1,20 +1,20 @@
-const assert = require('assert')
-const CNJokes = require('./index.js')
+const assert = require("assert");
+const CNJokes = require("./index.js");
 
-describe('get chucknorris joke', function() {
-  it('should return random joke', function() {
-
-  });
-
-  it('should return count jokes', function() {
+describe("get chucknorris joke", function() {
+  it("should return random joke", function() {
 
   });
 
-  it('should return random jokes', function() {
+  it("should return count jokes", function() {
 
   });
 
-  it('should return categories', function() {
+  it("should return random jokes", function() {
+
+  });
+
+  it("should return categories", function() {
 
   });
 });
