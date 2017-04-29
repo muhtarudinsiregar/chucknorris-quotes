f71eefd8060f2170c87041107aad560286ad04a5
muhtarudinsiregar <muhtarudinsiregar93@gmail.com>
2017-04-29 21:26:30 +0700

    Add test

--- tests.js
+++ tests.js
@@ -1,20 +1,47 @@
-const assert = require("assert");
+const chai = require("chai");
 const CNJokes = require("./index.js");
 
-describe("get chucknorris joke", function() {
-  it("should return random joke", function() {
+const sinon     = require("sinon");
 
+describe("get chucknorris joke", function() {
+  let sandbox;
+  let server;
+  beforeEach(() => {
+    expect = chai.expect;
+    assert = chai.assert;
+  });
+
+
+  it("should return one random joke", function() {
+    CNJokes.getRandomJoke()
+      .then((response) => {
+        expect(response.data.type).to.equal("success");
+      });
   });
 
   it("should return count jokes", function() {
-
+     CNJokes.getNumberOfJokes()
+      .then((response) => {
+        assert.isNumber(response.data.value);
+        
+        expect(response.data.type).to.equal("success");
+      });
   });
 
   it("should return random jokes", function() {
-
+     CNJokes.getRandomJokes()
+      .then((response) => {
+        
+        expect(response.data.type).to.equal("success");
+      });
   });
 
   it("should return categories", function() {
+     CNJokes.getCategories()
+      .then((response) => {
+        assert.isArray(response.data.value);
 
+        expect(response.data.type).to.equal("success");
+      });
   });
 });
